31e95410abb4917b4a1d884861ef8829798d6c4b
GitHub Action <action@github.com>
2022-03-14 21:15:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/soarex16/intellijlua/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/soarex16/intellijlua/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.soarex16.intellijlua.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.soarex16.intellijlua.MyBundle
 
 class MyApplicationService {
 
